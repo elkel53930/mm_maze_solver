6851c0f3eff5f059079d9b3ce90e43ff58851954
Kazuki Iida <elkel53930@gmail.com>
2023-06-27 08:10:50 +0900

    Implement Adachi-method.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod maze;
 mod solver;
 use std::str;
 
-use maze::Maze;
+use maze::{Maze, TOZAINANBOKU};
 use solver::{StepMap, StepMapMode};
 
 use crate::solver::decide_direction;
@@ -22,23 +22,33 @@ fn main() {
     simulate(&actual_maze, goal_x, goal_y);    
 }
 
+fn update_wall(actual_maze: &Maze, row: usize, col: usize, local_maze :&mut Maze)
+{
+    for d in TOZAINANBOKU {
+        local_maze.set_wall(row, col, d, actual_maze.get(row, col)[d]);
+    }
+}
 
 fn simulate(actual_maze: &Maze, goal_x: usize, goal_y: usize)
 {
     let mut x: usize = 0;
     let mut y: usize = 0;
     let mut stepmap = StepMap::new();
+    let mut local_maze = Maze::new();
 
     while x != goal_x || y != goal_y {
         let dir_to_go = 
-            decide_direction(&actual_maze, goal_x, goal_y, 
+            decide_direction(&local_maze, goal_x, goal_y, 
                 y, x, &mut stepmap);
 
-        let mut update_x:isize;
-        let mut update_y:isize;
+        let update_x:isize;
+        let update_y:isize;
         (update_x, update_y) = maze::nsew_to_index(dir_to_go);
         x = ((x as isize) + update_x) as usize;
         y = ((y as isize) + update_y) as usize;
+
+        update_wall(&actual_maze, y, x, &mut local_maze);
+
         println!("d: {:?}, x: {}, y: {}",dir_to_go, x, y);
     }
 }
